8c484a7a458bd501b27e021e7eabf56da7d0eea9
Danny Spangenberg <danny.spangenberg@twiceware.de>
2025-06-06 14:06:56 +0200

    Registry for tooltip, base-button, button

--- lib/registry/gsap-components.ts
+++ lib/registry/gsap-components.ts
@@ -14,6 +14,47 @@ export const gsapComponentRegistries: RegistryItem[] = [
     ]
   },
   {
+    name: 'tooltip',
+    type: 'registry:ui',
+    dependencies: ['react-aria-components'],
+    files: [
+      {
+        path: 'twc-ui/tooltip.tsx',
+        type: 'registry:ui',
+        target: '~/components/twc-ui/tooltip.tsx'
+      }
+    ]
+  },
+  {
+    name: 'base-button',
+    type: 'registry:ui',
+    dependencies: ['@hugeicons/react', 'react-aria-components', 'lucide-react'],
+    registryDependencies: [],
+    files: [
+      {
+        path: 'twc-ui/button.tsx',
+        type: 'registry:ui',
+        target: '~/components/twc-ui/base-button.tsx'
+      }
+    ]
+  },
+  {
+    name: 'button',
+    type: 'registry:ui',
+    dependencies: [],
+    registryDependencies: [
+      'https://ui.twiceware.cloud/r/twc-ui/base-button',
+      'https://ui.twiceware.cloud/r/twc-ui/tooltip'
+    ],
+    files: [
+      {
+        path: 'twc-ui/button.tsx',
+        type: 'registry:ui',
+        target: '~/components/twc-ui/button.tsx'
+      }
+    ]
+  },
+  {
     name: 'reveal-text',
     type: 'registry:ui',
     dependencies: ['gsap', '@gsap/react'],
